ea71454e0e31926f48e103b89c5a118ac1a249ab
Manuele longo <126604785+mlongo03@users.noreply.github.com>
2023-05-08 19:31:33 +0200

    Add files via upload

---
+++ so_long/mapcheck.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapcheck.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlongo <mlongo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 11:01:38 by mlongo            #+#    #+#             */
+/*   Updated: 2023/05/08 17:35:40 by mlongo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	error()
+{
+	printf("Errore\nla mappa é sbagliata");
+	exit (1);
+}
+
+void	error_name_map()
+{
+	printf("Errore\nil nome della mappa é sbagliato");
+	exit (1);
+}
+
+int	get_data_row_map(t_map *datamap, char *str)
+{
+	int i;
+
+	i = 0;
+	datamap->one = 0;
+	if (str[0] != '1' || str[ft_strlen(str) - 1] != '1')
+		error();
+	while (str[i])
+	{
+		if (str[i] == '1')
+			datamap->one += 1;
+		else if (str[i] == 'P')
+			datamap->P += 1;
+		else if (str[i] == 'C')
+			datamap->C += 1;
+		else if (str[i] == 'E')
+			datamap->E += 1;
+		else if (str[i] != '0' && str[i] != 'N')
+			error();
+		i++;
+	}
+	return (i);
+}
+
+void	check_map(t_data *data)
+{
+	t_map	datamap;
+	int		i;
+
+	i = 0;
+	data->datamap.widht = get_data_row_map(&data->datamap, data->datamap.splitmap[i]);
+	if (data->datamap.widht != data->datamap.one)
+		error();
+	i++;
+	while (data->datamap.splitmap[i])
+	{
+		if (data->datamap.widht != get_data_row_map(&data->datamap, data->datamap.splitmap[i++]))
+			error();
+	}
+	if (data->datamap.widht != data->datamap.one)
+		error();
+	if (i == data->datamap.widht || (data->datamap.C == 0 || data->datamap.P == 0 || data->datamap.E == 0))
+		error();
+	data->datamap.height = i;
+}
+
+void	check_map_name(char *str, int ret)
+{
+	if (ft_strnstr(str, "maps/", 5) == 0 || ret == -1)
+		error_name_map();
+}
---
+++ so_long/render_map.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render_map.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlongo <mlongo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 12:36:39 by mlongo            #+#    #+#             */
+/*   Updated: 2023/05/08 19:29:39 by mlongo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	create_map(t_data *data)
+{
+	int i;
+	int j;
+	int x;
+	int y;
+
+	i = 0;
+	j = 0;
+	y = 0;
+	x = 0;
+	if (data->datamap.C == 0)
+		data->datamap.splitmap[data->datamap.ROWE][data->datamap.COLUMNE] = 'E';
+	while (data->datamap.splitmap[i])
+	{
+		j = 0;
+		x = 0;
+		while (data->datamap.splitmap[i][j])
+		{
+			if (data->datamap.splitmap[i][j] == '1')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.one, x, y);
+			if (data->datamap.splitmap[i][j] == '0')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.zero, x, y);
+			if (data->datamap.splitmap[i][j] == 'C')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.C, x, y);
+			if (data->datamap.splitmap[i][j] == 'E')
+			{
+				data->datamap.ROWE = i;
+				data->datamap.COLUMNE = j;
+				if (data->datamap.C == 0)
+					mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.E, x, y);
+				else
+					mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.zero, x, y);
+			}
+			if (data->datamap.splitmap[i][j] == 'P')
+			{
+				data->datamap.ROWP = i;
+				data->datamap.COLUMNP = j;
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.P, x, y);
+			}
+			if (data->datamap.splitmap[i][j] == 'N')
+				mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.N, x, y);
+			x += 64;
+			j++;
+		}
+		y += 64;
+		i++;
+	}
+}
+
+void	render_map(t_data *data)
+{
+	int	widhtblock;
+	int	heightblock;
+
+	widhtblock = 64;
+	heightblock = 64;
+	data->mlx_ptr = mlx_init();
+	data->win_ptr = mlx_new_window(data->mlx_ptr, (data->datamap.widht * 64), (data->datamap.height * 64), "so_long");
+	data->img.P = mlx_xpm_file_to_image(data->mlx_ptr, "./images/Personaggio.xpm", &widhtblock, &heightblock);
+	data->img.C = mlx_xpm_file_to_image(data->mlx_ptr, "./images/upallon.xpm", &widhtblock, &heightblock);
+	data->img.E = mlx_xpm_file_to_image(data->mlx_ptr, "./images/scudetto.xpm", &widhtblock, &heightblock);
+	data->img.N = mlx_xpm_file_to_image(data->mlx_ptr, "./images/arbitro.xpm", &widhtblock, &heightblock);
+	data->img.zero = mlx_xpm_file_to_image(data->mlx_ptr, "./images/background.xpm", &widhtblock, &heightblock);
+	data->img.one = mlx_xpm_file_to_image(data->mlx_ptr, "./images/Wall.xpm", &widhtblock, &heightblock);
+	data->img.P = mlx_xpm_file_to_image(data->mlx_ptr, "./images/Personaggio.xpm", &widhtblock, &heightblock);
+	mlx_hook(data->win_ptr, 17, 0, close_window, data);
+	create_map(data);
+}
